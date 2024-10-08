f0b8e28e9c5dfa37f2d04f8bca3831e2f63f15fe
Michael Gowie <michaelgowie1@gmail.com>
2024-10-08 13:06:02 +0100

    Renames main file code file to hangman-game.py and adds to word list. Also updates README file.

--- README.md
+++ README.md
@@ -2,9 +2,33 @@
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+
 ---
-milestone_2 contains the function which generates a _random_ word
----
-milestone_3 contains the functions which obtain a guessed letter from the user and check whether it is contained in the guess
----
-milestone_4 contains the definition of the Hangman class. it contains the methods which are required during the game.
+## Usage instructions
+Running the file hangman-game.py will run the game for the user once, with a randomly selected word out of a list of 20 fruits and sports. Instructions to play will appear in the terminal.
+## Installation Instructions
+The required files may be found on the GitHub repository found at https://github.com/michaelgowie/hangman822.git
+## File Structure
+The file hangman-game.py is the one which contains the code to play the game. README.md contains a description of the project. The files milestone_2.py, milestone_3.py and milestone_4.py contain code which was used during development and testing.
+## Licensing
+MIT License
+
+Copyright (c) 2024 Michael Gowie
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ hangman-game.py
@@ -0,0 +1,98 @@
+import random as ran
+
+possible_word_list = ['Banana', 'Kiwi', 'Raspberry', 'Avocado', 'Nectarine', 'Apple', 'Peach', 'Mango', 'Strawberry', 'Plum', 'Cricket', 'Football', 'Grape', 'Pineapple', 'Tennis', 'Baseball', 'Rugby', 'Basketball', 'Volleyball', 'Grapefruit']
+
+
+class Hangman:
+    '''
+    This class facilitates the playing of the game hangman. An instance of this class represents a gae of hangman.
+
+    Attributes:
+        word (str): this is the word the user is trying to guess.
+        word_guessed (list): what the user is shown during playing, contains underscores where unguessed letters are and guessed letters.
+        num_letters (int): the number of remianing letters to guess.
+        num_lives (int): the number of lives the player has remaining.
+        word_list (list): the list of words the computer randomly chooses from.
+        list_of_guesses: a list which contains all guesses the player has made.
+    
+    '''
+    def __init__(self, word_list, num_lives  = 5):
+        '''
+        This chooses the random word, formulates the word_guessed list per the length of the word sets the attributes to their start values.
+        '''
+        self.word = ran.choice(word_list).lower()
+        self.word_guessed = ['_' for i in range(len(self.word))]
+        self.num_letters = len(set(list(self.word)))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+    def check_guess(self, letter):
+        '''
+        This method dictates what is done to the game state after a specific guess.
+
+        If the letter is correct, the num_letters variable is updated and the letter is placed into the word_guessed list in the correct place(s). 
+        If incorrect, the number of lives is reduced by one. In either case, information avout the success of the guess is printed.  
+        '''
+        if letter:
+            letter.lower()
+        if letter in self.word:
+            print(f'Good guess, {letter} is in the word.')
+            letter_idx_list = self.get_letter_indices(letter)
+            for idx in letter_idx_list:
+                self.word_guessed[idx] = letter
+            self.num_letters -= 1
+        else:
+            print(f'Unlucky, {letter} is not in the word')
+            self.num_lives -= 1
+            print(f'You have {self.num_lives} remaining lives.')
+    def ask_for_input(self):
+        '''
+        This method obtains a letter from the user to guess.
+
+        The method will only accept a single chracter which is in th ealphabet and has not been guessed previously. If these aren't met, the method will ask the user for a different input.
+        '''
+        while True:
+            user_guessed_letter = input('Guess a letter.')
+
+            if len(user_guessed_letter) != 1 or user_guessed_letter.isalpha() == False:
+                print('Please input exactly one letter')
+            elif user_guessed_letter in self.list_of_guesses:
+                print(f'You already guessed {user_guessed_letter}! Guess something else.')
+            else: 
+                print('Good guess!')
+                break
+        self.list_of_guesses.append(user_guessed_letter)
+        return user_guessed_letter
+    def get_letter_indices(self, letter):
+        '''
+        This method obtains a list of indices which a certain letter appears in the word to be guessed. It is used in the check_guess method. 
+        '''
+        current_idx = 0
+        list_of_indices = []
+        while True:
+            if letter in self.word[current_idx:]:
+                list_of_indices.append(self.word.find(letter, current_idx))
+                current_idx = self.word.find(letter, current_idx) + 1
+            else:
+                break
+        return list_of_indices
+
+
+def play_game(list_of_words):
+    game = Hangman(list_of_words)
+    while True:
+        if game.num_lives == 0:
+            print(f'You are out of lives ya blody idiot! The word was {game.word}. blody heal')
+            break
+        if game.num_letters == 0:
+            life_plural = 'life'
+            if game.num_lives > 1:
+                life_plural = 'lives'
+            print(f'Congratulations! You won the game with {game.num_lives} {life_plural} remaining! The word was {game.word}.')
+            break
+        word_guess_str = ','.join(game.word_guessed)
+        print(f'The word is : {word_guess_str}, you have {game.num_lives} lives remaining.')
+        guess = game.ask_for_input()
+        game.check_guess(guess)
+
+play_game(possible_word_list)
